7dbf59d6636c171e00ccf74fb31f2c49f0791e48
AlexDjinn <motodjeen@gmail.com>
2017-04-02 18:03:49 +0300

    готовый компас

---
+++ src/compass.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+function setDirectionName(name){
+    $('.compass .direction').text(name);
+}
+function setAngle(degree){
+    degree = parseInt(degree, 10) || 0;
+    $('.compass .angle').text(degree);
+}
+function rotateCompass(angle){
+    $('.compass .disc').css({
+        transform: 'rotate('+ angle +'deg)'
+    });
+}
+
+$(window).on('deviceorientation', function(event){
+    event = event.originalEvent
+    // event.alpha
+    // event.beta
+    // event.gamma
+    rotateCompass(event.alpha)
+    setAngle(event.alpha)
+    // $('.compass').css({
+        // transform: 'rotate3d(1,0,0,'+ (event.beta*-1) +'deg)'
+    // });
+})
